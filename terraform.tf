84074c8c0e41f94ca34e627240bbbbbe672f6d67
praskill1086-commits <praskill1086@gmail.com>
2026-02-27 11:10:12 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-/* Uncomment this block to use Terraform Cloud for this tutorial
+Uncomment this block to use Terraform Cloud for this tutorial
 cloud {
-    organization = "organization-name"
+    organization = "pra-org"
     workspaces {
       name = "learn-terraform-sensitive-variables"
     }
 }
-*/
+
 required_providers {
     aws = {
       source  = "hashicorp/aws"
